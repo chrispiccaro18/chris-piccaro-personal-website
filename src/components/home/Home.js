bcb620cdebb9db3ce3334823c76768c68f898a11
chrispiccaro <chrispiccaro18@gmail.com>
2019-08-22 20:27:23 -0700

    About

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.scss';
 import Hero from './hero/Hero';
+import About from './about/About';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
